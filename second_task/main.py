65be55267b459f141f5bb6a5aef5622f628b3ced
Kirill <pnchkirill@gmail.com>
2022-05-26 01:09:29 +0300

    Code refactoring

--- second_task/main.py
+++ second_task/main.py
@@ -1,53 +1,49 @@
-import time
-
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.animation import FuncAnimation
 from matplotlib.axes import Axes
 
 
-def customize_first_figure(axis: Axes):
+def update_first_func(frame, line, x):
+    line.set_ydata(np.cos(2 * (x + frame)))
+    return [line]
+
+
+def customize_axis(axis: Axes):
     axis.grid()
     axis.set_xlim(-10, 10)
     axis.set_ylim(-10, 10)
-    axis.set_title("Перші два графіки", fontsize=16)
-    axis.set_xlabel("Ocь Х")
-    axis.set_ylabel("Ocь Y")
-    axis.legend(loc='upper left')
-
-
-def customize_second_figure(axis: Axes):
-    axis.grid()
-    axis.set_xlim(-10, 10)
-    axis.set_ylim(-10, 10)
-    axis.set_title("Сума двох графикiв", fontsize=16)
     axis.set_xlabel("Ocь Х")
     axis.set_ylabel("Ocь Y")
     axis.legend(loc='upper left')
 
 
 def func_2(x):
-    return [2 * i for i in x]
+    return [2 * x - 4 for x in x]
 
 
-fx, axes = plt.subplots(1, 2, dpi=80, facecolor='#f0f0f0', edgecolor='k')
+# ------- Data for the task -------
+fig, axes = plt.subplots(1, 2, dpi=80, facecolor='#f0f0f0', edgecolor='k')
 
 x_1 = np.arange(-10, 10, 0.1)
 x_2 = [i for i in range(-10, 10)]
 x_3 = np.arange(-10, 10, 0.1)
 
-function_1 = lambda x: np.sin(2 * x)
+function_1 = np.cos(2 * x_1)
 function_2 = func_2
-function_3 = lambda x: np.array(x_3) * 2 + np.sin(2 * x)
+function_3 = lambda x: (np.array(x) * 2 - 4) + np.cos(2 * x)
+# ------------------------------
 
-axes[0].plot(
-    x_1, function_1(x_1),
-    label='f(x) = sin(2x)',
+# ------- Creating Axes -------
+func_1, = axes[0].plot(
+    x_1, function_1,
+    label='f(x) = cos(2x)',
     color='#fcba03',
     linewidth=2,
 )
 axes[0].plot(
     x_2, function_2(x_2),
-    label='f(x) = 2х',
+    label='f(x) = 2x - 4',
     color='#eb363f',
     linewidth=1,
     linestyle='--',
@@ -57,26 +53,30 @@ axes[0].plot(
 )
 axes[1].plot(
     x_3, function_3(x_3),
-    label='f(x) = sin(2x) + 2x',
+    label='f(x) = cos(2x) + 2x - 4',
     linestyle='-.'
 )
-customize_first_figure(axes[0])
-customize_second_figure(axes[1])
+# ------------------------------
 
-plt.ion()
-for delay in np.arange(0, np.pi, 0.1):
-    plt.clf()
+# ------- Axes and Figure Customization-------
+fig.canvas.manager.set_window_title('Завдання №2')
 
-    function_1 = lambda x: np.sin(2 * (x + delay))
-    axes[0].plot(
-        x_1, function_1(x_1),
-        label='f(x) = sin(2x)',
-        color='#fcba03',
-        linewidth=2,
-    )
-    plt.draw()
-    plt.gcf().canvas.flush_events()
-    time.sleep(0.02)
+axes[0].set_title('Перші два графіки', fontsize=16)
+axes[1].set_title('Сума графикiв', fontsize=16)
+customize_axis(axes[0])
+customize_axis(axes[1])
+# ------------------------------
 
-plt.ioff()
+# ------- An First Axis Animation -------
+animation = FuncAnimation(
+    fig,
+    func=update_first_func,
+    frames=np.arange(0, 4 * np.pi, 0.1),
+    fargs=(func_1, x_1),
+    interval=30,
+    blit=True,
+    repeat=True,
+)
+# ------------------------------
+
 plt.show()
